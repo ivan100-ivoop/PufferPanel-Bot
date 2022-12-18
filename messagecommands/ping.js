2bb2ce7805e8c9904e7153d26069314a283e1ba3
ivan100-ivoop <83706102+ivan100-ivoop@users.noreply.github.com>
2022-12-18 17:26:43 +0200

    Delete ping.js

--- messagecommands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "ping",
-    aliases: [''], 
-    run: async (client, message, args)=>{
-        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-    }
-};
