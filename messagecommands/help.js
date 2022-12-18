48cebb2d34af1236e460fcaf4db6f50148a76b4a
ivan100-ivoop <83706102+ivan100-ivoop@users.noreply.github.com>
2022-12-18 17:41:37 +0200

    Update help.js

--- messagecommands/help.js
+++ messagecommands/help.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const { color, bot } = require('./../config.json');
 
 module.exports = (client, message, args) => {
-    const commands = `${bot.prefix}user help\` - list with account commands \n\`${bot.prefix}server help\` - list with server commands \n\``;    
+    const commands = `${bot.prefix}user help\` - list with account commands \n\`${bot.prefix}server help\` - list with server commands \n\`${bot.prefix}about\` - About of me. \n\``;    
     message.channel.send({
         embeds:[
             new EmbedBuilder()
