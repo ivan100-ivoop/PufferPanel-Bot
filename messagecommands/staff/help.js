4f8b74986f59eccc53c2b4d5bec0134fb9c63642
ivan100-ivoop <83706102+ivan100-ivoop@users.noreply.github.com>
2022-12-18 13:25:30 +0200

    Update help.js

--- messagecommands/staff/help.js
+++ messagecommands/staff/help.js
@@ -4,7 +4,7 @@ const { color, bot, staff } = require('./../../config.json');
 module.exports = (client, message, args) => {
     const user = message.mentions.users.first() || message.author
     if(!staff.admin.includes(user.id) && staff.owner !== user.id) return message.reply(`:x: You dont have permission to use this command!`)
-    const commands = `${bot.prefix}staff count\` - list of servers and users \n\`${bot.prefix}staff delete <id>\` - unlink user server \n\`${bot.prefix}staff list <user>\` - list user server \n\`${bot.prefix}staff status <id>\` - iunteract with user server \n\``;    
+    const commands = `${bot.prefix}staff count\` - list of servers and users \n\`${bot.prefix}staff delete <id>\` - unlink user server \n\`${bot.prefix}staff list <user>\` - list user and servers \n\`${bot.prefix}staff status <id>\` - interact with user servers \n\`${bot.prefix}staff unlink <user>\` - unlink user Account \n\``;    
     message.channel.send({
         embeds:[
             new EmbedBuilder()
